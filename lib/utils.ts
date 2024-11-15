8e8c00a05e646df5780c18f6458c619f6459a7a9
venkatreddy <venkatreddynareddula@gmail.com>
2024-11-15 23:19:20 +0530

    Implement PlaidLink and Dwolla

--- lib/utils.ts
+++ lib/utils.ts
@@ -203,7 +203,7 @@ export const authFormSchema = (type: string) => z.object({
   address1: type === 'sign-in' ? z.string().optional() :  z.string().max(50),
   city: type === 'sign-in' ? z.string().optional() :  z.string().max(50),
   state: type === 'sign-in' ? z.string().optional() : z.string().max(20),
-  postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(6).max(6),
+  postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(5).max(5),
   dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
   ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
   // both sign in and sign up
